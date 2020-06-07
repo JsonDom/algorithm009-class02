0d867f43681af4aa647bd0803b436b0e48f86b1c
Jayden <894688321@qq.com>
2020-06-08 00:46:14 +0800

    第三周作业及总结提交

---
+++ Week_03/lowestCommonAncestor.java
@@ -0,0 +1,39 @@
+package com.example.algorithm;
+
+/**
+ * @author wangzj
+ * @description 68.二叉树的最近公共祖先
+ *
+ * 示例 1:
+ *
+ * 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+ * 输出: 3
+ * 解释: 节点 5 和节点 1 的最近公共祖先是节点 3。
+ * 示例 2:
+ *
+ * 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+ * 输出: 5
+ * 解释: 节点 5 和节点 4 的最近公共祖先是节点 5。因为根据定义最近公共祖先节点可以为节点本身。
+ *
+ * @date 2020/6/4 9:43
+ */
+public class lowestCommonAncestor {
+    /**
+     * 树结构
+     */
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null) return right; //如果左节点没有，则pq同在右侧
+        if (right == null) return left; //如果右节点没有，则pq同在左侧
+        return root;
+    }
+}
