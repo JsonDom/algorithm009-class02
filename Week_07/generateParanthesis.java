c6efdd7f637e5aa7c3aaa6ec32fc9fb8b2efbf4c
Jayden <894688321@qq.com>
2020-07-05 23:01:53 +0800

    第七周作业

---
+++ Week_07/generateParanthesis.java
@@ -0,0 +1,53 @@
+package com.example.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author wangzj
+ * @description 22.括号生成
+ *
+ * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+ *
+ *  
+ *
+ * 示例：
+ *
+ * 输入：n = 3
+ * 输出：[
+ *        "((()))",
+ *        "(()())",
+ *        "(())()",
+ *        "()(())",
+ *        "()()()"
+ *      ]
+ *
+ * @date 2020/7/5 22:48
+ */
+public class generateParanthesis {
+    List<String> res = new ArrayList<>();
+    public List<String> generateParenthesis(int n) {
+        dfs(n, n, "");
+        return res;
+    }
+
+    private void dfs(int left, int right, String curStr) {
+        if (left == 0 && right == 0) { // 左右括号都不剩余了，递归终止
+            res.add(curStr);
+            return;
+        }
+
+        if (left > 0) { // 如果左括号还剩余的话，可以拼接左括号
+            dfs(left - 1, right, curStr + "(");
+        }
+        if (right > left) { // 如果右括号剩余多于左括号剩余的话，可以拼接右括号
+            dfs(left, right - 1, curStr + ")");
+        }
+    }
+
+    public static void main(String[] args) {
+        int num = 3;
+        generateParanthesis generate = new generateParanthesis();
+        System.out.println(generate.generateParenthesis(3));
+    }
+}
